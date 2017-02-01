54ec232d3553f0352726e161e0c4094580343d78
sharangw <sharang.wakankar@hotmail.com>
2017-02-01 14:57:59 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+$(document).ready(function() { // called after DOM is done being built
+		
+	$("#submitButton").click(function() {
+		
+		var input = $("#userInput").val();
+		
+		$.ajax({
+			method: "GET",
+			url: "http://pokeapi.co/api/v2/pokemon/" + input + "/",
+			success: function(data) {
+				
+				$("#pokeName").html(data.name);
+			}
+		});
+		
+	});
+	
+	
+});
