b49b3e7e70f4ab5a43be51eb22f2cfbe21b8a4c3
sharangw <sharang.wakankar@hotmail.com>
2017-02-05 01:30:37 -0700

    Update with Team Rocket stuff

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,7 @@ $(document).ready(function() { // called after DOM is done being built
 	var name1 = "", name2 = "";	
 	var moves1 = 0, moves2 = 0, timeout = 0; 
 	var chance1 = 0, chance2 = 0;
+	var trocket = false, flag1 = false;
 		
 	$("#submitButton1").click(function() {
 		
@@ -19,31 +20,11 @@ $(document).ready(function() { // called after DOM is done being built
 			url: "https://pokeapi.co/api/v2/pokemon/" + p1 + "/",
 			success: function(data) {				
 				name1 = data.name;
-				//$("#poke1").html(name1);
+				console.log(name1);
+				//$("#poke1").html(name1);				
+				
 			}
 		});	
-		
-		// GET ABILITIES
-		$.ajax({ 
-			method: "GET",
-			url: "https://pokeapi.co/api/v2/pokemon/" + p1 + "/",
-			success: function(data) {
-				
-				var abilArray = data.abilities;
-				var ab1 = [];
-		    	var j = 0;
-				
-				for (var i=0; i < abilArray.length; i++) {
-					ab1[j] = abilArray[i].ability.name;
-					var eachrow = "<tr>"
-		                 + "<td>" + i + "</td>"
-		                 + "<td>" + ab1[j] + "</td>"
-		                 + "</tr>";
-					$('#tbody1').append(eachrow);
-					j++;
-				}
-			}
-		});
 		
 	}); // end button1 click
 	
@@ -54,119 +35,17 @@ $(document).ready(function() { // called after DOM is done being built
 		// GET NAME
 		$.ajax({
 			method: "GET",
-			url: "https://pokeapi.co/api/v2/pokemon/" + p2 + "/",
+			url: "httpss://pokeapi.co/api/v2/pokemon/" + p2 + "/",
 			success: function(data) {	
 				name2 = data.name;
+				console.log(name2)
 				//$("#poke2").html(name2);
 			}
 		});	
 		
-		// GET ABILITIES
-		$.ajax({
-			method: "GET",
-			url: "https://pokeapi.co/api/v2/pokemon/" + p2 + "/",
-			success: function(data) {
-				
-				var abilArray = data.abilities;
-				var ab2 = [];
-		    	var j = 0;
-				
-				for (var i=0; i < abilArray.length; i++) {
-					ab2[j] = abilArray[i].ability.name;
-					var eachrow = "<tr>"
-		                 + "<td>" + i + "</td>"
-		                 + "<td>" + ab2[j] + "</td>"
-		                 + "</tr>";
-					$('#tbody2').append(eachrow);
-					j++;
-				}
-			}
-		});
-		
     }); // end button2 click
-    
-    $("#abilButton1").click(function() {     	
-    	
-    	var ab1 = "";
-    	var randomAb1 = $("#randomAb1").val();
-    	
-    	// GET CHOSEN ABILITY
-    	$.ajax({
-			method: "GET",
-			url: "https://pokeapi.co/api/v2/pokemon/" + p1 + "/",
-			success: function(data) {				
-				ab1 = data.abilities[randomAb1].ability.name;
-				$("#poke11").html(name1);
-				$("#ab1").html(ab1);
-			}
-		});    	
-    	
-    }); // end abilButton1 
-    
-    $("#abilButton2").click(function() {   	
-    	
-    	var ab2 = "";
-    	var randomAb2 = $("#randomAb2").val();
-    	
-    	// GET CHOSEN ABILITY
-    	$.ajax({
-			method: "GET",
-			url: "https://pokeapi.co/api/v2/pokemon/" + p2 + "/",
-			success: function(data) {				
-				ab2 = data.abilities[randomAb2].ability.name;
-				$("#poke22").html(name2);
-				$("#ab2").html(ab2);
-			}
-		});
-    	
-    	
-     }); // end abilButton1
 		
     $("#prepbattleButton").click(function() {
-    	var moves1 =[];
-    	var moves2 =[];
-    	
-    	// GET MOVES
-    	$.ajax({
-			method: "GET",
-			url: "https://pokeapi.co/api/v2/pokemon/" + p1 + "/",
-			success: function(data) {				
-				m1 = data.moves;	
-				
-				var myMoves = [];
-				var j = 0;
-				for (var i=0; i<4; i++) {
-					myMoves[j] = m1[i].move.name;
-					j++;
-				}					
-				$("#m0").html(myMoves[0]);
-				$("#m1").html(myMoves[1]);
-				$("#m2").html(myMoves[2]);
-				$("#m3").html(myMoves[3]);
-			}
-			
-		});
-    	
-    	// GET MOVES
-    	$.ajax({
-			method: "GET",
-			url: "https://pokeapi.co/api/v2/pokemon/" + p2 + "/",
-			success: function(data) {				
-				m2 = data.moves;	
-				
-				var myMoves = [];
-				var j = 0;
-				for (var i=0; i<4; i++) {
-					myMoves[j] = m2[i].move.name;
-					j++;
-				}					
-				$("#m4").html(myMoves[0]);
-				$("#m5").html(myMoves[1]);
-				$("#m6").html(myMoves[2]);
-				$("#m7").html(myMoves[3]);
-			}
-			
-		});
     	
     	// GET IMAGE
     	$.ajax({
@@ -203,10 +82,78 @@ $(document).ready(function() { // called after DOM is done being built
 				img.src = 'model/'+id2+'.png';	
 			}
 		});
+    	
+    	console.log("name1: "+name1 + " name2: "+ name2);
+    	
+    	if (name1 === "meowth" || name2 === "meowth" || p1 === "meowth" || p1 === 52 || p2 === "meowth" || p2 === 52) {
+    		
+    		trocket = true;
+    		
+    		teamRocket();
+    	}
 		
     }); // end prepbattleButton click
     
-    $("#battleButton").click(function() {  
+    function teamRocket() {
+    	
+    	var img = new Image();
+		var div = document.getElementById('jessie');
+		
+		img.onload = function() {
+			  div.appendChild(img);
+			};
+		
+		img.src = 'model/jessie.png';
+		
+		var img2 = new Image();
+		var div2 = document.getElementById('james');
+		
+		img2.onload = function() {
+			  div2.appendChild(img2);
+			};
+		
+		img2.src = 'model/james.png';
+    	
+    }
+    
+    $("#battleButton").click(function() {     	
+    	
+    	if(name1 === "meowth") {
+    		
+    		flag1 = true;
+    	}    		
+    	
+    	if (trocket === true) {
+    		
+    		if (flag1 === true) {
+    			
+    			$("#moveTR").html("Oh no! Team Rocket steals " + name2 + " and flees..");
+    			
+    			var gotta = document.getElementById("theme");
+    			gotta.pause();
+    			
+    			var denounce = document.getElementById("rocketMotto");
+    			denounce.play();
+    			
+    		} else {
+    			
+    			$("#moveTR").html("Oh no! Team Rocket steals " + name1 + " and flees..");
+    			
+    			var gotta = document.getElementById("theme");
+    			gotta.pause();
+    			
+    			var denounce = document.getElementById("rocketMotto");
+    			denounce.play();
+    		}    		
+    		   		
+    	} else {
+    		
+    		battle();
+    	}    	
+    	
+     }); // end battleButton click
+    
+    function battle() {
     	
     	moves1 = setInterval(function(){ 
     		printAttackOne()
@@ -214,7 +161,7 @@ $(document).ready(function() { // called after DOM is done being built
     	   	
     	timeout = setTimeout(attackTwoTimeout, 2000);
     	
-     }); // end battleButton click
+    }
 		
     function randomNumberTo10() {
     	
@@ -287,9 +234,23 @@ $(document).ready(function() { // called after DOM is done being built
     	
     	clearInterval(moves2);
     	
-    	clearTimeout(timeout);
+    	clearTimeout(timeout);    	    	
     	
-    	var exp1 = 0, exp2 = 0, stat1 = 0, stat2 = 0;	    	
+    	if(trocket === true) {
+    		
+    		rocketVictory();
+    		
+    	} else {
+    		
+    		getStats();
+    	}   	  	
+    	
+    	
+    }); //end stopButton click
+    
+    function getStats() {
+    	
+    	var exp1 = 0, exp2 = 0, stat1 = 0, stat2 = 0;	
     	
     	$.ajax({
     		method: "GET",
@@ -318,17 +279,12 @@ $(document).ready(function() { // called after DOM is done being built
     			}    			
     			
     			calculateVictor(exp1, exp2, stat1, stat2, chance1, chance2);
-    			
-    			/*console.log(stat1);
-    			console.log(stat2);
-    			console.log(chance1);
-    			console.log(chance2);*/
+  
     		}
     		
-    	});   	
-    	
-    	
-    }); //end stopButton click
+    	}); 
+    
+    } // end getStats
     
     
     function calculateVictor(expA, expB, statA, statB, chanceA, chanceB) {
@@ -356,6 +312,20 @@ $(document).ready(function() { // called after DOM is done being built
 			$("#pokeWinner").html("It was a tie!");
 			$("#pokeLoser").html("[ "+totalPoints1 + " points]");
 		}
+    	
+    } // end calculateVictor
+    
+    function rocketVictory() {
+    
+    	if (flag1 === true) {
+    		
+    		$("#pokeWinner").html("Player 2, you lost your pokemon to Team Rocket!");
+    	
+    	} else {
+    		
+    		$("#pokeWinner").html("Player 1, you lost your pokemon to Team Rocket!");
+    	}
+    
     	
     }
 	
